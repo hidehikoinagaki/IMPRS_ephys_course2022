8d9ee53c4d00d83009e68bcd5e74a831eb79bdae
hidehikoinagaki <57011270+hidehikoinagaki@users.noreply.github.com>
2022-09-07 11:42:08 -0400

    Add files via upload

--- all_compiled.m
+++ all_compiled.m
@@ -13,26 +13,26 @@
 plot_raster
 
 %%
-% 2. Estimate mean spike rate for each trial type as sr(R), sr(L)
-% 3. Plot mean spike rate for different trial types
-% 4. Compute selectivity as sr(R) - sr(L)
+% 2. Estimate and plot mean spike rate for each trial type as sr(R), sr(L)
+% 3. Compute selectivity as sr(R) - sr(L)
 plot_PSTH_with_selectivity
 
 
 %%
-% 5. Error trial
+% 4. Error trial
 % Change the name of variable in each function and explore the error trials
 
 %% Session-based analysis
 
 %%
-% 6. Grand average (average across trials and neurons) ...
-% 7. Grand average selectivity and its absolute value
+% 5. Grand average (average across trials and neurons) ...
+% 6. Grand average selectivity and its absolute value
 plot_session_PSTH_with_selectivity
 
 
 %% Dimensionality reduction
 
 %%
-% 8. Compute the coding direction (CD) and direction with max variance orthognal to the coding direction
+% 7. Perform PCA
+% 8. Compute the coding direction (CD) 
 plot_PCA_CD
